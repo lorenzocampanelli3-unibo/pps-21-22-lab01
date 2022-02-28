9e63937bf38be6a8e6fb516836a80a36a394e57a
Lorenzo Campanelli <lorenzo.campanelli3@studio.unibo.it>
2022-02-28 11:23:19 +0100

    Add new tests and classes for SimpleBankAccountWithAtm

---
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -0,0 +1,50 @@
+package lab01.example.model;
+
+/**
+ * This class represent a particular instance of a BankAccount.
+ * In particular, a Simple Bank Account allows always the deposit
+ * while the withdrawal is allowed only if the balance greater or equal the withdrawal amount
+ */
+public class SimpleBankAccountWithAtm implements BankAccount {
+
+    public static final double FEE = 1;
+
+    private double balance;
+    private final AccountHolder holder;
+
+    public SimpleBankAccountWithAtm(final AccountHolder holder, final double balance) {
+        this.holder = holder;
+        this.balance = balance;
+    }
+    @Override
+    public AccountHolder getHolder(){
+        return this.holder;
+    }
+
+    @Override
+    public double getBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public void deposit(final int userID, final double amount) {
+        if (checkUser(userID)) {
+            this.balance += amount;
+        }
+    }
+
+    @Override
+    public void withdraw(final int userID, final double amount) {
+        if (checkUser(userID) && isWithdrawAllowed(amount)) {
+            this.balance -= amount + FEE;
+        }
+    }
+
+    private boolean isWithdrawAllowed(final double amount){
+        return this.balance >= amount + FEE;
+    }
+
+    private boolean checkUser(final int id) {
+        return this.holder.getId() == id;
+    }
+}
---
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
@@ -0,0 +1,59 @@
+import lab01.example.model.AccountHolder;
+import lab01.example.model.BankAccount;
+import lab01.example.model.SimpleBankAccount;
+import lab01.example.model.SimpleBankAccountWithAtm;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * The test suite for testing the SimpleBankAccountWithAtm implementation
+ */
+class SimpleBankAccountWithAtmTest {
+
+    public static final double FEE = 1;
+
+    private AccountHolder accountHolder;
+    private BankAccount bankAccount;
+
+    @BeforeEach
+    void beforeEach(){
+        accountHolder = new AccountHolder("Mario", "Rossi", 1);
+        bankAccount = new SimpleBankAccountWithAtm(accountHolder, 0);
+    }
+
+    @Test
+    void testInitialBalance() {
+        assertEquals(0, bankAccount.getBalance());
+    }
+
+    @Test
+    void testDeposit() {
+        bankAccount.deposit(accountHolder.getId(), 100);
+        assertEquals(100, bankAccount.getBalance());
+    }
+
+    @Test
+    void testWrongDeposit() {
+        bankAccount.deposit(accountHolder.getId(), 100);
+        bankAccount.deposit(2, 50);
+        assertEquals(100, bankAccount.getBalance());
+    }
+
+    @Test
+    void testWithdraw() {
+        final int depositedAmount = 100;
+        final int withdrawnAmount = 70;
+        bankAccount.deposit(accountHolder.getId(), depositedAmount);
+        bankAccount.withdraw(accountHolder.getId(), withdrawnAmount);
+        assertEquals(depositedAmount-withdrawnAmount-FEE, bankAccount.getBalance());
+    }
+
+    @Test
+    void testWrongWithdraw() {
+        bankAccount.deposit(accountHolder.getId(), 100);
+        bankAccount.withdraw(2, 70);
+        assertEquals(100, bankAccount.getBalance());
+    }
+}
